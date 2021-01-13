c225bfe622c6bb00baea3c3060e04f56f821644c
Studenov Roman <studenovroman@yandex.ru>
2021-01-13 19:27:14 +0700

    HOMEWORK-1: feature добавил скрипт read для чтения директорий

--- src/read.js
+++ src/read.js
@@ -1,40 +1,45 @@
 const fs = require('fs');
 const path = require('path');
-const { Command } = require('commander');
-
-function myParseInt(value) {
-  return parseInt(value);
-}
 
 /* Обязательный аргумент передается просто ссылкой, например /home/roman/Документы/Git/Homework/ */
 /* Глубина передается, через опциональный флаг --depth или -d */
 
-const program = new Command();
-program.version('1.0.0').option('-d, --depth <depth>', 'глубина отображения дерева', myParseInt, 3);
-program.parse(process.argv);
+const arguments = process.argv.slice(2);
+const pathToDir = arguments.find((elem) => !elem.includes('--depth=') || !elem.includes('-d='));
 
-if (!program.args[0]) {
+if (!pathToDir) {
   return console.error('🤯🤯🤯', '\x1b[31m', 'Не указан обязательный аргумент "путь до директории"');
 }
 
-const currentPath = path.resolve(program.args[0]);
+const d = Number.parseInt(arguments.find((elem) => elem.includes('--depth=') || elem.includes('-d='))?.split('=')[1] || 0);
+const depth = d < 0 || isNaN(d) ? 0 : d;
+
+const currentPath = path.resolve(pathToDir);
 
 function IterateTree(pathTree, number) {
   let iterate = number;
-  fs.readdirSync(pathTree).forEach((elem) => {
-    fs.stat(path.resolve(pathTree, elem), (error, stats) => {
-      if (stats.isFile()) {
-        console.log('-'.repeat(iterate), elem);
-      }
-      if (stats.isDirectory() && iterate <= program.depth) {
-        console.log('-'.repeat(iterate), '/', elem);
-        iterate = iterate + 1;
-        const localPath = path.resolve(pathTree, elem);
-        IterateTree(localPath, iterate);
-      } else if (iterate > program.depth) {
-        console.log('-'.repeat(iterate), '/', elem);
-      }
-    });
+
+  fs.readdirSync(pathTree).forEach((elem, index) => {
+    if (index === 0) {
+      iterate = iterate + 1;
+    }
+
+    const stats = fs.statSync(path.resolve(pathTree, elem));
+    const isDirectory = stats.isDirectory();
+    const isFile = stats.isFile();
+    const symbolIterate = iterate - 1;
+
+    if (isFile) {
+      console.log('-'.repeat(symbolIterate), elem);
+    }
+
+    if (isDirectory && iterate <= depth) {
+      console.log('-'.repeat(symbolIterate), '/', elem);
+      const localPath = path.resolve(pathTree, elem);
+      IterateTree(localPath, iterate);
+    } else if (isDirectory) {
+      console.log('-'.repeat(symbolIterate), '/', elem);
+    }
   });
 }
 
